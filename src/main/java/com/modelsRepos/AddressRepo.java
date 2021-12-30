1ee4aec5273f82a136622455501c15fd31de7654
karolina <nogacka.karolina@gmail.com>
2021-12-30 20:41:41 +0100

    add query to AddressRepo

--- src/main/java/com/modelsRepos/AddressRepo.java
+++ src/main/java/com/modelsRepos/AddressRepo.java
@@ -1,6 +1,7 @@
 package com.modelsRepos;
 
 import com.model.Address;
+import com.model.PersonalData;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,4 +11,18 @@ import java.util.List;
 
 @Repository
 public interface AddressRepo extends CrudRepository<Address, Long> {
+
+    @Query("SELECT a FROM Address a WHERE " +
+            "LOWER(a.country) LIKE LOWER(:addressCountry)" +
+            "AND LOWER(a.city) LIKE LOWER(:addressCity)" +
+            "AND LOWER(a.postcode) LIKE LOWER(:addressPostcode)" +
+            "AND LOWER(a.street) LIKE LOWER(:addressStreet)" +
+            "AND (a.buildingNr = :addressBuildingNr)" +
+            "AND (a.apartmentNr = :addressApartmentNr)")
+    List<Address> searchForAddress(@Param("addressCountry") String addressCountry,
+                                   @Param("addressCity") String addressCity,
+                                   @Param("addressPostcode") String addressPostcode,
+                                   @Param("addressStreet") String addressStreet,
+                                   @Param("addressBuildingNr") int addressBuildingNr,
+                                   @Param("addressApartmentNr") int addressApartmentNr);
 }
