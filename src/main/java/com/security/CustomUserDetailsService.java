f08161e641be2a195c2a707389833f18d8e13cfa
Qwertiush <63415593+Qwertiush@users.noreply.github.com>
2022-01-04 20:42:42 +0100

    Blocking user after 3 tries

--- src/main/java/com/security/CustomUserDetailsService.java
+++ src/main/java/com/security/CustomUserDetailsService.java
@@ -2,23 +2,48 @@ package com.security;
 
 import com.controller.UserController;
 import com.model.User;
+import com.security.PreventBruteForce.LoginAttemptService;
+import com.vaadin.flow.component.notification.Notification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class CustomUserDetailsService implements UserDetailsService
 {
+
     @Autowired
     private UserController userController;
 
+    @Autowired
+    private LoginAttemptService loginAttemptService;
+
+    @Autowired
+    private HttpServletRequest request;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //Sprawdzenie czy IP jest zablokowane
+        String ip = getClientIP();
+        if (loginAttemptService.isBlocked(ip)) {
+            throw new RuntimeException("blocked");
+        }
+        //
         User user = userController.getUserById(username);
         if(user == null){
             throw new UsernameNotFoundException("User not found");
         }
 
         return new CustomUserDetails(user);
+    }
+
+    private String getClientIP() {
+        String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader == null){
+            return request.getRemoteAddr();
+        }
+        return xfHeader.split(",")[0];
     }
 }
